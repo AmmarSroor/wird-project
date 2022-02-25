aacb4e553d50eb40dcc73d2a75cbd9875fb3e4b1
AmmarSroor <91069802+AmmarSroor@users.noreply.github.com>
2022-02-26 00:13:06 +0300

    Add files via upload

---
+++ lib/cubit/home_page_cubit/home_page_state.dart
@@ -0,0 +1,21 @@
+part of 'home_page_cubit.dart';
+
+abstract class HomePageState extends Equatable {
+  @override
+  List<Object> get props => [];
+}
+class HomePageInitialState extends HomePageState {}
+
+class HomePageBottomNavLoadingState extends HomePageState {}
+class HomePageBottomNavLoadedState extends HomePageState {}
+
+class HomePageLoadingState extends HomePageState {}
+class HomePageLoadedState extends HomePageState {}
+class HomePageErrorState extends HomePageState {}
+
+class DrawerLoadingState extends HomePageState {}
+class DrawerLoadedState extends HomePageState {}
+class DrawerErrorState extends HomePageState {}
+
+class HomePageSliderLoadingState extends HomePageState {}
+class HomePageSliderLoadedState extends HomePageState {}
